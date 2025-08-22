311452ce0016b27c5a3aad5bcd2cff1fe1f76890
Kaidon456 <annilador456@gmail.com>
2025-08-22 16:39:23 -0600

    Base de datos con Flask

---
+++ mi_flask_app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@127.0.0.1/mi_flask_app'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # Aquí puedes añadir más configuraciones
